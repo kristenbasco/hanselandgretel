7a01fbed7d50ae5d66c87fb65397dbf6c96a6160
rllhenson <rllhenson@gmail.com>
2015-03-06 03:32:42 -0500

    7 and 8

--- js/codepen2.js
+++ js/codepen2.js
@@ -335,21 +335,21 @@ $(function() {
 
 	// scene 5, 4 technically
 	.add(TweenMax.to('#scene5', 1, { 
-		transform: 'translateY(0)'
+		transform: 'translateY(0)',
+		delay: 5
 	}))
 	.add(TweenMax.to('#scene5', 4, {
 		opacity: 1,
-		delay: 0.5
 	}))
 
 
 	// scene 6, 5 technically
 	.add(TweenMax.to('#scene6', 1, { 
-		transform: 'translateY(0)'
+		transform: 'translateY(0)',
+		delay: 5
 	}))
 	.add(TweenMax.to('#scene6', 4, {
 		opacity: 1,
-		delay: 0.5
 	}))
 
 
@@ -410,22 +410,104 @@ $(function() {
 
 	// 7
 	.add(TweenMax.to('#scene7', 1, { 
-		transform: 'translateY(0)'
+		transform: 'translateY(0)',
+		delay: 5
 	}))
 	.add(TweenMax.to('#scene7', 4, {
-		opacity: 1,
-		delay: 0.5
+		opacity: 1
 	}))
+	// text one for this scene
+	.add(TweenMax.to('#scene7 img#running_trees', 1, {
+		transform: 'translateX(0)',
+		opacity: .9,
+		delay: 5
+	}))
+	.add(TweenMax.to('#scene7 img#running_house', 1, {
+		transform: 'translateX(0)',
+		opacity: 1,
+		ease: Linear.easeNone
+	}))
+	.add(TweenMax.to('#scene7 img#running_hansel', 1, {
+		transform: 'translateX(0)',
+		opacity: 1,
+		delay: 5
+	}))
+	.add(TweenMax.to('#scene7 img#running_gretel', 1, {
+		transform: 'translateX(0)',
+		opacity: 1,
+		delay: 5
+	}))
+	.add(TweenMax.to('#scene7 img#running_trees1', 1, {
+		transform: 'translateX(0)',
+		opacity: 1,
+		delay: 5
+	}))
+		// text one for this scene
+	.add(TweenMax.to('#scene7 h3#backtoforest', 4, {
+		opacity: 1,
+		delay: 5
+	}))
+	.add(TweenMax.to('#scene7 h3#backtoforest', 1, {
+		opacity: 0,
+		delay: 5
+	}))
+
+
 
 
 	// scene 8
-	.add(TweenMax.to('#scene8', 1, { 
-		transform: 'translateY(0)'
+	.add(TweenMax.to('#scene8', 4, { 
+		transform: 'translateY(0)',
+		delay: 5
 	}))
-	.add(TweenMax.to('#scene8', 4, {
-		opacity: 1,
-		delay: 0.5
+	.add(TweenMax.to('#scene8', 4, { 
+		opacity: 1
 	}))
+	// .add(TweenMax.to('#scene8 img#endtree4', 1, {
+	// 	opacity: 1
+	// }))
+	// .add(TweenMax.to('#scene8 img#endtree3', 1, {
+	// 	opacity: 1
+	// }))
+	// .add(TweenMax.to('#scene8 img#endtree2', 1, {
+	// 	opacity: 1
+	// }))
+	// .add(TweenMax.to('#scene8 img#endtree1', 1, {
+	// 	opacity: 1
+	// }))	
+	.add(TweenMax.to('#scene8 img#hug', 1, {
+		opacity: 1
+	}))
+	.add(TweenMax.to('#scene8 h3', 1, {
+		opacity: 0
+	}))
+	.add(TweenMax.to('#scene8 img#endtree2', 1, {
+		transform: "translateX(-800px)"
+	}))
+	.add(TweenMax.to('#scene8 img#endtree1', 1, {
+		transform: "translateX(1000px)"
+	}))
+	.add(TweenMax.to('#scene8 img#endtree3', 1, {
+		transform: "translateX(700px)"
+	}))
+	.add(TweenMax.to('#scene8 h2', 1, {
+		opacity: 1
+	}))
+	// .add(TweenMax.to('#scene8 img#endtree4', 1, {
+	// 	transform: "translateX(-700px)"
+	// }))	
+
+	// .add(TweenMax.to('#scene8 h3#happyend', 4, {
+	// 	opacity: 1,
+	// 	delay: 5
+	// }))
+	// .add(TweenMax.to('#scene8 h3#happyend', 1, {
+	// 	opacity: 0
+	// }))
+	// .add(TweenMax.to('#scene8 h3#end', 4, {
+	// 	opacity: 1,
+	// 	delay: 5
+	// }))
 	;
 // 
 	// .add(TweenMax.to('#scene2 img#gretel_walking', 3, {
